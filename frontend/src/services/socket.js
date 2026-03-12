468e47a22cd9b8e1369aac8e59297e12d09ce9bc
Ivin Shajan <ivinshajan@gmail.com>
2026-03-12 15:24:56 +0530

    update

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:4000";
-
+console.log(SOCKET_URL);
 let socket = null;
 
 export function getSocket() {
   if (!socket) {
-    socket = io(SOCKET_URL, { 
+    socket = io(SOCKET_URL, {
       transports: ["websocket"],
       extraHeaders: {
         "ngrok-skip-browser-warning": "69420"
